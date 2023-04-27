fdb72ca8ff898431d9511a9d3d680c2e9dfb30bd
Jason <jason5donoghue@gmail.com>
2023-04-27 12:09:50 +0100

    basic test file created

--- birdBytes_project/src/new.test.js
+++ birdBytes_project/src/new.test.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import { test, expect } from "@jest/globals";
-import { Root } from "./routes/root.jsx";
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import { test, expect } from "@jest/globals";
+// import { Root } from "./routes/root.jsx";
 
-test("renders the new button", function () {
-  render(<Root />);
-  const linkElement = screen.getByText(/New/i);
-  expect(linkElement).toBeInTheDocument();
-}
-)
+
+// write a test that checks if the root component renders and the new button shows using vitest
+
+import { test, expect } from "vitest"
+
+test("1+1", ()=>{
+    expect(1+1).toEqual(3)
+})
