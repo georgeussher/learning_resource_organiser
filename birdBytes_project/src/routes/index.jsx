0d29a45e3dac2c3a9e52146a8d8800b9bcc1cf2c
georgeussher <gussher@icloud.com>
2023-04-27 15:05:44 +0100

    index page

--- birdBytes_project/src/routes/index.jsx
+++ birdBytes_project/src/routes/index.jsx
@@ -1,13 +1,19 @@
+import soc_logo from './soc_logo.png'
+import studyosaurus from './studyosaurus_logo.png'
+
 export default function Index() {
     return (
-      <p id="zero-state">
-        This is a School of Code App.
-        <br />
-        Check out{" "}
-        <a href="https://www.schoolofcode.co.uk/">
-          to join us!
-        </a>
-        .
-      </p>
+      <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', height: '100vh' }}>
+        <p>
+        </p>
+        <div>
+          <img src={soc_logo} alt="SOC logo"/>
+        </div> 
+        <div>
+          <img src={studyosaurus} alt="studyosaurus logo" style={{ width: '90%', margin: '5%' }}/>
+        </div>
+        <p>
+        </p>
+    </div>
     );
   }
