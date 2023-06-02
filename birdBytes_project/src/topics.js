4c5529801d7dc4584cd26110ce7dea4b8d58caea
georgeussher <gussher@icloud.com>
2023-06-02 14:20:39 +0100

    deleted redundant code

--- birdBytes_project/src/topics.js
+++ birdBytes_project/src/topics.js
@@ -56,20 +56,3 @@ function set(topics) {
   return localforage.setItem("topics", topics);
 }
 
-// fake a cache so we don't slow down stuff we've already seen
-// let fakeCache = {};
-
-// async function fakeNetwork(key) {
-//   if (!key) {
-//     fakeCache = {};
-//   }
-
-//   if (fakeCache[key]) {
-//     return;
-//   }
-
-//   fakeCache[key] = true;
-//   return new Promise(res => {
-//     setTimeout(res, Math.random() * 800);
-//   });
-// }
